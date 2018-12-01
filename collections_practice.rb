7f455a6fb1cbb44ed3bb38316ef4239c15bda8b2
Dalya Eidda <dalia.eidda@gmail.com>
2018-12-01 15:10:24 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,37 @@
-
+def sort_array_asc(array)
+  array.sort
+end
+def sort_array_desc(array)
+  array.sort.reverse
+end 
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.size <=> b.size
+  end 
+end 
+def swap_elements(array)
+  array[1], array[2] = array[2],array[1]
+  array
+end 
+def reverse_array(array)
+  array.reverse  do |a, b| 
+    a<=>b 
+  end 
+end 
+def kesha_maker(array)
+array.each {|name| name[2]="$"}  
+end 
+def find_a(array)
+  array.select {|word| word.start_with?("a")}
+end 
+def sum_array(array)
+  array.inject { |sum, n| sum + n }
+end 
+def add_s(array)
+  array.each do |word|
+   word + "s"
+   if word.index == 1 
+     return word 
+   end 
+  end 
+end 
